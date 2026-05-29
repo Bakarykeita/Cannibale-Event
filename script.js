300d3eee611c557d27a091255d763760b01ede48
BakaryKeita <lunickeita17@gmail.com>
2026-05-29 22:33:24 +0000

    deuxième

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const galeries = {
     opinel: {
         titre: "OPINEL 12 SAGATALA — En concert",
-        photos: ["image/a.jpg", "image/b.jpg", "image/c.jpg", "image/d.jpg"]
+        photos: ["image/All-White/1.jpeg", "image/All-White/2.jpeg", "image/All-White/3.jpeg", "image/All-White/4.jpeg", "image/All-White/5.jpeg", "image/All-White/6.jpeg", "image/All-White/7.jpeg", "image/All-White/8.jpeg", "image/All-White/9.jpeg", "image/All-White/10.jpeg"]
     },
     blackwhite: {
         titre: "SOIRÉE BLACK AND WHITE",
-        photos: ["image/a.jpg", "image/b.jpg", "image/c.jpg", "image/d.jpg"]
+        photos: ["image/Black-and-White/1.jpeg", "image/Black-and-White/2.jpeg", "image/Black-and-White/3.jpeg", "image/Black-and-White/4.jpeg", "image/Black-and-White/5.jpeg", "image/Black-and-White/6.jpeg", "image/Black-and-White/7.jpeg", "image/Black-and-White/8.jpeg", "image/Black-and-White/9.jpeg", "image/Black-and-White/10.jpeg", "image/Black-and-White/11.jpeg", "image/Black-and-White/12.jpeg", "image/Black-and-White/13.jpeg", "image/Black-and-White/14.jpeg", "image/Black-and-White/15.jpeg", "image/Black-and-White/16.jpeg", "image/Black-and-White/17.jpeg", "image/Black-and-White/18.jpeg", "image/Black-and-White/19.jpeg", "image/Black-and-White/20.jpeg", "image/Black-and-White/21.jpeg", "image/Black-and-White/22.jpeg", "image/Black-and-White/23.jpeg", "image/Black-and-White/24.jpeg", "image/Black-and-White/25.jpeg", "image/Black-and-White/26.jpeg", "image/Black-and-White/27.jpeg", "image/Black-and-White/28.jpeg", "image/Black-and-White/29.jpeg", "image/Black-and-White/30.jpeg", "image/Black-and-White/31.jpeg", "image/Black-and-White/32.jpeg", "image/Black-and-White/33.jpeg", "image/Black-and-White/34.jpeg", "image/Black-and-White/35.jpeg", "image/Black-and-White/36.jpeg", "image/Black-and-White/37.jpeg", "image/Black-and-White/38.jpeg", "image/Black-and-White/39.jpeg", "image/Black-and-White/40.jpeg", "image/Black-and-White/41.jpeg", "image/Black-and-White/42.jpeg"]
     },
     allwhite: {
         titre: "SOIRÉE ALL WHITE",
-        photos: ["image/a.jpg", "image/b.jpg", "image/c.jpg", "image/d.jpg"]
+        photos: ["image/All-White/1.jpeg", "image/All-White/2.jpeg", "image/All-White/3.jpeg", "image/All-White/4.jpeg", "image/All-White/5.jpeg", "image/All-White/6.jpeg", "image/All-White/7.jpeg", "image/All-White/8.jpeg", "image/All-White/9.jpeg", "image/All-White/10.jpeg"]
     }
 };
 
